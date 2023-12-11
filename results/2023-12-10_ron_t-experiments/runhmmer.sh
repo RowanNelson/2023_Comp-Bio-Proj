c3eb31c4629f3f990fb96e315a9ad69a0b9e35e9
Rowan Nelson <rowanon@Rowans-MacBook-Pro.local>
2023-12-11 18:07:11 -0500

    test t values

---
+++ results/2023-12-10_ron_t-experiments/runhmmer.sh
@@ -0,0 +1,24 @@
+
+hmmsearch ../../data/PF03421.hmm ../../data/YopJ_Positive.fasta > pfam_positive.out
+hmmsearch ../../data/PF03421.hmm ../../data/YopJ_Negative.fasta > pfam_negative.out
+hmmsearch ../../data/PF03421.hmm ../../data/YopJ_animal-pathogen.fasta > pfam_animal.out
+
+hmmsearch output_t0.01.hmm ../../data/YopJ_Positive.fasta > 0.01_positive.out
+hmmsearch output_t0.01.hmm ../../data/YopJ_Negative.fasta > 0.01_negative.out
+hmmsearch output_t0.01.hmm ../../data/YopJ_animal-pathogen.fasta > 0.01_animal.out
+
+hmmsearch output_t0.03.hmm ../../data/YopJ_Positive.fasta > 0.03_positive.out
+hmmsearch output_t0.03.hmm ../../data/YopJ_Negative.fasta > 0.03_negative.out
+hmmsearch output_t0.03.hmm ../../data/YopJ_animal-pathogen.fasta > 0.03_animal.out
+
+hmmsearch output_t0.05.hmm ../../data/YopJ_Positive.fasta > 0.05_positive.out
+hmmsearch output_t0.05.hmm ../../data/YopJ_Negative.fasta > 0.05_negative.out
+hmmsearch output_t0.05.hmm ../../data/YopJ_animal-pathogen.fasta > 0.05_animal.out
+
+hmmsearch output_t0.08.hmm ../../data/YopJ_Positive.fasta > 0.08_positive.out
+hmmsearch output_t0.08.hmm ../../data/YopJ_Negative.fasta > 0.08_negative.out
+hmmsearch output_t0.08.hmm ../../data/YopJ_animal-pathogen.fasta > 0.08_animal.out
+
+hmmsearch output_t0.1.hmm ../../data/YopJ_Positive.fasta > 0.1_positive.out
+hmmsearch output_t0.1.hmm ../../data/YopJ_Negative.fasta > 0.1_negative.out
+hmmsearch output_t0.1.hmm ../../data/YopJ_animal-pathogen.fasta > 0.1_animal.out
